77d854089ef6fda46dcc9b19c1cc2c95b70a3e02
brad <brad@brad.tw>
2019-11-29 20:03:14 +0800

    3

--- app/src/main/java/tw/org/iii/android201905/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201905/MainActivity.java
@@ -10,55 +10,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView tv;
-    private UIHandler uiHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.tv);
-        uiHandler = new UIHandler();
     }
 
-    public void test1(View view) {
-        MyThread mt1 = new MyThread("A");
-        mt1.start();
+    public void clickLeft(View view) {
     }
-
-    private class MyThread extends Thread {
-        String name;
-        MyThread(String name){this.name = name;}
-        @Override
-        public void run() {
-            for (int i=0; i<10; i++){
-                Log.v("brad", name + ": i = " + i);
-                Message message = new Message();
-                Bundle data = new Bundle();
-                data.putString("name", name);
-                data.putInt("i" , i);
-                message.setData(data);
-                uiHandler.sendMessage(message);
-                try {
-                    Thread.sleep(500);
-                }catch (Exception e){
-
-                }
-            }
-        }
+    public void clickRight(View view) {
     }
-
-    private class UIHandler extends Handler {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            Bundle data = msg.getData();
-            String name = data.getString("name");
-            int i = data.getInt("i");
-            tv.setText(name + ": i = " + i);
-        }
-    }
-
-
 }
